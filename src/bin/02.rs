fc5278b2d335bde1c3a7b770b12ad0c282fcb14d
Pia <gayeongparkk@gmail.com>
2023-12-02 20:01:32 +0900

    day2 part2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, hash::Hash};
+
 use regex::Regex;
 
 advent_of_code::solution!(2);
@@ -14,13 +16,12 @@ pub fn part_one(input: &str) -> Option<u32> {
             .as_str()
             .parse()
             .unwrap();
-        println!("answer:{:?}", answer);
+
         let packs = game_split.next().unwrap().split(';');
         let mut game_is_valid = true;
         for pack in packs {
             let mut pack_is_valid = true;
             for cube in pack.split(',') {
-                println!("{}", cube);
                 let mut cube_split = cube.split(' ');
                 let _ = cube_split.next().unwrap();
                 let count: u32 = cube_split.next().unwrap().parse().unwrap();
@@ -58,7 +59,53 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let games = input.split('\n');
+    let mut sum = 0;
+    for game in games {
+        let mut game_split = game.split(':');
+        let re = Regex::new(r"(\d+)").unwrap();
+        let answer: u32 = re
+            .find(game_split.next().unwrap())
+            .unwrap()
+            .as_str()
+            .parse()
+            .unwrap();
+
+        let packs = game_split.next().unwrap().split(';');
+        let mut cube_hashmap: HashMap<&str, u32> = HashMap::new();
+
+        for pack in packs {
+            for cube in pack.split(',') {
+                println!("hashmap:{:?}", cube_hashmap);
+                let mut cube_split = cube.split(' ');
+                let _ = cube_split.next().unwrap();
+                let count: u32 = cube_split.next().unwrap().parse().unwrap();
+                let color = cube_split.next().unwrap();
+                let min_value = cube_hashmap.get(color);
+                match min_value {
+                    Some(min_value) => {
+                        // println!("{:?}", min_value);
+                    }
+                    None => {
+                        cube_hashmap.insert(color, count);
+                        continue;
+                    }
+                }
+                if *min_value.unwrap() < count {
+                    cube_hashmap.insert(color, count);
+                }
+            }
+        }
+
+        let mut mul = 1;
+        for (key, value) in cube_hashmap.iter() {
+            //println!("{}{}", key, value);
+            mul *= value;
+        }
+        sum += mul;
+    }
+
+    Some(sum)
 }
 
 #[cfg(test)]
